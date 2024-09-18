cc551ded640aec25fd40121db262aee4a0363143
Vivek Chanpuriya <chanpuriyavivek@hotmail.com>
2024-09-18 23:37:40 +0530

    initial commit

---
+++ src/routes/index.jsx
@@ -0,0 +1,13 @@
+export default function Index() {
+    return (
+      <p id="zero-state">
+        Welcome to Keystone Contact Manager
+        <br />
+        {/* Check out{" "}
+        <a href="https://reactrouter.com">
+          the docs at reactrouter.com
+        </a> */}
+        .
+      </p>
+    );
+  }
